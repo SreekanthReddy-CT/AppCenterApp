6ba3cd91b3edc688294b83ef2137a13aa0a5116f
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2023-01-10 17:58:24 +0530

    Initial commit

---
+++ AppCenterApp/AppCenterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCenterAppApp.swift
+//  AppCenterApp
+//
+//  Created by YML on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AppCenterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
